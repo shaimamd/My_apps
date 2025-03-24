d8e05c11c28af01118a8f6e09a21bc908f117dd2
shaimamd <106159672+shaimamd@users.noreply.github.com>
2025-03-24 11:14:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('🎈Happy World')
 
-st.title('🎈 App Name')
+st.write('Hello world:)')*
 
-st.write('Hello world!')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+col1, col2 =st.columns(2)            
+with col1:            
+    x=st.slider("Choose an x value",1,10)
+with col2: 
+    st.write("The value of :blue[****x****] is",x)
+st.subheader("Area Chart")
+chart_data = pd.DataFrame(np.random.randn(50, 3), columns=["A", "B", "C"])
+st.area_chart(chart_data)   
